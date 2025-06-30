155d1489ab2e1a791045afce69432747b4372167
Jhonathan Carvalho <ti.dev.carvalho@gmail.com>
2025-06-30 15:50:41 -0300

    Ajustes

--- src/services/BuildStoreEnterprise.Identidade.API/Models/UserViewModels.cs
+++ src/services/BuildStoreEnterprise.Identidade.API/Models/UserViewModels.cs
@@ -10,7 +10,7 @@ namespace BuildStoreEnterprise.Identidade.API.Models
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
-        public string Senha { get; set; }
+        public string? Senha { get; set; }
 
         [Compare("Senha", ErrorMessage = "As senhas não conferem.")]
         public string SenhaConfirmacao { get; set; }
