f615a8a106ced41078760db30bb770c0e07ff572
Jhonathan Carvalho <ti.dev.carvalho@gmail.com>
2025-07-03 21:08:37 -0300

    Adicionada a configurações do program

---
+++ src/services/BuildStoreEnterprise.Identidade.API/Configuration/SwaggerConfig.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace BuildStoreEnterprise.Identidade.API.Configuration
+{
+    public static class SwaggerConfig
+    {
+        public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(name: "v1", new OpenApiInfo
+                {
+                    Title = "BuildStore Enterprise Identity API",
+                    Description = "Esta API faz parte do projeto BuildStore Enterprise.",
+                    Contact = new OpenApiContact() { Name = "Jhonathan Carvalho", Email = "ti.dev.carvalho@gmail.com" },
+                    License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
+                });
+
+            });
+           
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerConfiguration(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+
+            return app;
+        }
+    }
+}
